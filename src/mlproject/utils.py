4ab1d1999b9dcec1dfd1aa859079961cb17680a6
veer-patel <veerpatel66930@gmail.com>
2024-01-16 18:58:56 +0530

    data transformation

--- src/mlproject/utils.py
+++ src/mlproject/utils.py
@@ -5,6 +5,7 @@ from src.mlproject.logger import logging
 import pandas as pd
 from dotenv import load_dotenv
 import pymysql
+import pickle
 
 load_dotenv()
 
@@ -31,3 +32,19 @@ def read_sql_data():
 
     except Exception as e:
         raise CustomeException(e)
+    
+
+def save_object(file_path,obj):
+
+    try:
+        
+        dir_path = os.path.dirname(file_path)
+
+        os.makedirs(dir_path,exist_ok=True)
+
+        with open(file_path,"wb") as file_obj:
+            pickle.dump(obj,file_obj)
+
+
+    except Exception as e:
+        raise CustomeException(e,sys)
